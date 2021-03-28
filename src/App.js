7e289a212bbbe466a36a62406ec50812edb637b2
Sergio Colado <33748103+colado@users.noreply.github.com>
2021-03-28 17:41:56 +0300

    Set up project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  LoginButton,
+  Text,
+  useSession,
+  CombinedDataProvider,
+} from "@inrupt/solid-ui-react";
+
+const authOptions = {
+  clientName: "Solid Todo App",
+};
 
 function App() {
+  const { session } = useSession();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app-container">
+      {session.info.isLoggedIn ? (
+        <CombinedDataProvider
+          datasetUrl={session.info.webId}
+          thingUrl={session.info.webId}
         >
-          Learn React
-        </a>
-      </header>
+          <div className="message logged-in">
+            <span>You are logged in as: </span>
+            <Text
+              properties={[
+                "http://www.w3.org/2006/vcard/ns#fn",
+                "http://xmlns.com/foaf/0.1/name",
+              ]}
+            />
+          </div>
+        </CombinedDataProvider>
+      ) : (
+        <LoginButton
+          oidcIssuer="https://inrupt.net/"
+          redirectUrl={window.location.href}
+          authOptions={authOptions}
+        />
+      )}
     </div>
   );
 }
