a5b42df8ebfa24ecda933b6cbd8ae9f0cca2e508
Sergio Colado <33748103+colado@users.noreply.github.com>
2021-11-06 13:40:09 +0200

    Add search request

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 var clientID = "8a79d168a29043d292345c34a143e4f4";
 var clientSecret = "ed23fc81a15c4620a3750f6e4111be3e";
+const spotifyBaseUrl = "https://api.spotify.com/v1";
 
 export const getSpotifyAccessToken = async () => {
   const body = new URLSearchParams();
@@ -21,4 +22,24 @@ export const getSpotifyAccessToken = async () => {
   );
   res = await res.json();
   return res.access_token;
+};
+
+export const searchSpotifyItem = async (token, params) => {
+  let url = new URL(`${spotifyBaseUrl}/search`);
+  
+  for (const key in params) {
+    url.searchParams.set(key, params[key]);
+  }
+
+  const requestOptions = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
+    },
+  };
+
+  let res = await fetch(url, requestOptions);
+  res = await res.json();
+  console.log(res);
 };
